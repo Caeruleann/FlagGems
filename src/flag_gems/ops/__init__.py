caf64abd264fc8ca66386116f2a964ebb9a4a812
Caeruleann <hairui.wang@compiler-dev.com>
2026-06-26 10:58:44 +0800

    unsafe_index first implement

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -204,6 +204,7 @@ from flag_gems.ops.hypot import hypot, hypot_out
 from flag_gems.ops.i0 import i0, i0_out
 from flag_gems.ops.i0_ import i0_
 from flag_gems.ops.index import index
+from flag_gems.ops.unsafe_index import _unsafe_index
 from flag_gems.ops.index_add import index_add, index_add_
 from flag_gems.ops.index_copy_ import index_copy, index_copy_
 from flag_gems.ops.index_put import _index_put_impl_, index_put, index_put_
@@ -715,6 +716,7 @@ __all__ = [
     "i0",
     "i0_",
     "i0_out",
+    "_unsafe_index",
     "index",
     "index_add",
     "index_add_",
