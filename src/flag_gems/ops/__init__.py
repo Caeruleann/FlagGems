c358c2c88e583cd7bbe45c9cc29ca33ed711b916
Caeruleann <hairui.wang@compiler-dev.com>
2026-09-04 02:38:00 +0000

    fix code-style

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -531,8 +531,8 @@ from flag_gems.ops.linalg_matrix_sqrth import (
     linalg_matrix_sqrth,
     linalg_matrix_sqrth_out,
 )
-from flag_gems.ops.linalg_qr import linalg_qr, linalg_qr_out
 from flag_gems.ops.linalg_norm import linalg_norm
+from flag_gems.ops.linalg_qr import linalg_qr, linalg_qr_out
 from flag_gems.ops.linalg_slogdet import linalg_slogdet
 from flag_gems.ops.linalg_solve_triangular import (
     linalg_solve_triangular,
